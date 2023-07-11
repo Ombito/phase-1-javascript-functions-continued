c0b14964f4c65586e1312833e88abed12c56fa02
ombito <alvinombito09@gmail.com>
2023-07-12 00:50:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+function saturdayFun (activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+const mondayWork = function (activity = "go to the office"){
+return `This Monday, I will ${activity}.` 
+}
+function wrapAdjective (wraps){
+    return function (adjective) {
+        return `You are ${wraps}${adjective}${wraps}!`;
+    }
+}
+console.log(saturdayFun());
+  console.log(saturdayFun("bathe my dog")); 
+  
+  console.log(mondayWork());
+  console.log(mondayWork("work from home"));
+  
+  const wrapWithStars = wrapAdjective('*');
+  console.log(wrapWithStars("a hard worker"));
+  
+  const wrapWithBars = wrapAdjective("||");
+  console.log(wrapWithBars("a dedicated programmer"));
